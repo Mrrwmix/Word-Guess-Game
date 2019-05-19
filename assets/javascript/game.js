56ff1d29b7438b7d7b8dc303d373b59791893804
Matthew Winemiller <seidoc@gmail.com>
2019-05-18 22:33:52 -0700

    font, music, some style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var wins = 0;
 var blanks = [];
 var lettersGuessed = [];
 var guessesRemaining = 12;
-var tracks = ["assets/tunes/bomberman.mp3", "assets/tunes/chrono.mp3", "assets/tunes/kirby.mp3", "assets/tunes/ninja.mp3", "assets/tunes/scorpion.mp3", "assets/tunes/yoshi.mp3", "assets/tunes/zangief.mp3", "assets/tunes/zero.mp3", "assets/tunes/KI.mp3"];
+var tracks = ["assets/tunes/bomberman.mp3", "assets/tunes/chrono.mp3", "assets/tunes/kirby.mp3", "assets/tunes/ninja.mp3", "assets/tunes/scorpion.mp3", "assets/tunes/yoshi.mp3", "assets/tunes/zangief.mp3", "assets/tunes/zero.mp3", "assets/tunes/KI.mp3", "assets/tunes/loss.mp3"];
 var images = ["assets/images/bomberman.gif", "assets/images/crono.gif", "assets/images/kirby.gif", "assets/images/robo.gif", "assets/images/ryu.gif", "assets/images/scorpion.gif", "assets/images/yoshi.gif", "assets/images/zangief.gif", "assets/images/zero.gif", "assets/images/vile.gif", "assets/images/thunder.gif"];
 console.log(cpuChoice);
 
@@ -47,7 +47,7 @@ document.onkeyup = function (event) {
         }
     }
     else {
-        alert("Please guess a letter!");
+        document.getElementById("instructions").textContent = "Letters only!";
     }
 
 }
@@ -55,37 +55,37 @@ document.onkeyup = function (event) {
 
 //generate an image after win or loss
 function imageGenerator() {
-    if (cpuChoice == "bomberman"){
+    if (cpuChoice == "bomberman") {
         document.getElementById("results").setAttribute("src", images[0]);
     }
-    else if (cpuChoice == "crono"){
+    else if (cpuChoice == "crono") {
         document.getElementById("results").setAttribute("src", images[1]);
     }
-    else if (cpuChoice == "kirby"){
+    else if (cpuChoice == "kirby") {
         document.getElementById("results").setAttribute("src", images[2]);
     }
-    else if (cpuChoice == "robo"){
+    else if (cpuChoice == "robo") {
         document.getElementById("results").setAttribute("src", images[3]);
     }
-    else if (cpuChoice == "ryu"){
+    else if (cpuChoice == "ryu") {
         document.getElementById("results").setAttribute("src", images[4]);
     }
-    else if (cpuChoice == "scorpion"){
+    else if (cpuChoice == "scorpion") {
         document.getElementById("results").setAttribute("src", images[5]);
     }
-    else if (cpuChoice == "yoshi"){
+    else if (cpuChoice == "yoshi") {
         document.getElementById("results").setAttribute("src", images[6]);
     }
-    else if (cpuChoice == "zangief"){
+    else if (cpuChoice == "zangief") {
         document.getElementById("results").setAttribute("src", images[7]);
     }
-    else if (cpuChoice == "zero"){
+    else if (cpuChoice == "zero") {
         document.getElementById("results").setAttribute("src", images[8]);
     }
-    else if (cpuChoice == "vile"){
+    else if (cpuChoice == "vile") {
         document.getElementById("results").setAttribute("src", images[9]);
     }
-    else{
+    else {
         document.getElementById("results").setAttribute("src", images[10]);
     }
 }
@@ -102,6 +102,33 @@ function winGranter() {
         wins++;
         document.getElementById("winnies").innerText = wins;
         imageGenerator();
+        if (cpuChoice == "bomberman") {
+            playSound(tracks[0]);
+        }
+        else if (cpuChoice == "crono" || cpuChoice == "robo") {
+            playSound(tracks[1]);
+        }
+        else if (cpuChoice == "kirby") {
+            playSound(tracks[2]);
+        }
+        else if (cpuChoice == "ryu") {
+            playSound(tracks[3]);
+        }
+        else if (cpuChoice == "scorpion") {
+            playSound(tracks[4]);
+        }
+        else if (cpuChoice == "yoshi") {
+            playSound(tracks[5]);
+        }
+        else if (cpuChoice == "zangief") {
+            playSound(tracks[6]);
+        }
+        else if (cpuChoice == "zero" || cpuChoice == "vile") {
+            playSound(tracks[7]);
+        }
+        else {
+            playSound(tracks[8]);
+        }
         cpuChoice = choices[Math.floor(Math.random() * choices.length)].toLowerCase();
         blanker();
     }
@@ -110,18 +137,13 @@ function winGranter() {
 function lossGranter() {
     if (guessesRemaining <= 0) {
         document.getElementById("results").innerHTML = "<h2>The character was " + cpuChoice + "!</h2>";
+        playSound(tracks[tracks.length - 1]);
         cpuChoice = choices[Math.floor(Math.random() * choices.length)].toLowerCase();
         blanker();
     }
 }
 
 function playSound(url) {
-    var audio = document.createElement('audio');
-    audio.style.display = "none";
-    audio.src = url;
-    audio.autoplay = true;
-    audio.onended = function () {
-        audio.remove() //Remove when played.
-    };
-    document.body.appendChild(audio);
+    document.getElementById("audio").setAttribute("src", url);
+    document.getElementById("audio").setAttribute("autoplay", true);
 }
